96dd9310703d21175e3d0b5b54d95e06e9afa693
HerfstJ <Josh.Herfst@rabobank.nl>
2019-09-11 12:26:31 +0200

    presentation changes

---
+++ src/components/CartTotals.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+// import {BrowserRouter as Router, Link} from 'react-router-dom';
+import PayPalButton from './PayPalButton';
+
+export default class CartTotals extends Component {
+    render() {
+        const {cartSubTotal,cartTax, cartTotal,clearCart} = this.props && this.props.value;
+        return (
+           <React.Fragment>
+               <div className="container">
+                   <div className="row">
+                       <div className="col-10 mt-2 ml-sm-5 ml-md-auto col-sm-8 text-capitalize text-right">
+                                    <button onClick={() => clearCart()} type="button" className="btn btn-outline-danger text-uppercase mb-3 px-5">
+                                        clear cart
+                                    </button>
+                            <h5>
+                                <span className="text-title">
+                                    subtotal:
+                                </span>
+                                <strong>${cartSubTotal}</strong>
+                            </h5>
+                            <h5>
+                                <span className="text-title">
+                                    tax:
+                                </span>
+                                <strong>${cartTax}</strong>
+                            </h5>
+                            <h5>
+                                <span className="text-title">
+                                    total:
+                                </span>
+                                <strong>${cartTotal}</strong>
+                            </h5>
+                            <PayPalButton
+                                total={cartTotal}
+                                clearCart={clearCart}
+                                history={this.props.history}
+                            />
+                       </div>
+                   </div>
+               </div>
+           </React.Fragment>
+        );
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,22 +1,31 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-
-import styles from './styles.css'
-
-export default class ExampleComponent extends Component {
-  static propTypes = {
-    text: PropTypes.string
-  }
-
-  render() {
-    const {
-      text
-    } = this.props
-
-    return (
-      <div className={styles.test}>
-        Example Component: {text}
-      </div>
-    )
-  }
+import React, { Component } from 'react';
+import Title from './components/Title';
+import CartColumns from './components/CartColumns';
+import EmptyCart from './components/EmptyCart';
+import {ProductConsumer} from 'q-sig-context';
+import CartList from './components/CartList';
+import CartTotals from './components/CartTotals';
+export default class Cart extends Component {
+    render() {
+        return (
+            <section className="back-white">
+                { <ProductConsumer>
+                    {value => {
+                         const cart = value && value.cart;
+                        return (cart && cart.length > 0) ? (
+                            <React.Fragment>
+                                <Title name="your" title="cart"/>
+                                <CartColumns/>
+                                <CartList value={value} />
+                                <CartTotals
+                                value={value}
+                                history={this.props.history}
+                                />
+                            </React.Fragment>
+                        ) : <EmptyCart/>;
+                    }}
+                </ProductConsumer>}
+            </section>
+        )
+    }
 }
